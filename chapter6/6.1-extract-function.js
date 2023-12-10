fffc8925559effa36a9f4dc4cfe1c18e0e73a3fb
KANGHO <ariaskkh@gmail.com>
2023-12-10 15:15:21 +0900

    6.1 배너 출력 로직, 세부 사항 출력 로직을 함수로 추출

--- chapter6/6.1-extract-function.js
+++ chapter6/6.1-extract-function.js
@@ -3,9 +3,7 @@ import { MockInvoice, Today } from "./6.1-extract-function.fixture.js";
 function print0wing(invoice) {
   let outstanding = 0;
 
-  console.log("***************");
-  console.log("**** 고객 채무 ****");
-  console.log("***************");
+  printBanner();
 
   // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
@@ -22,10 +20,19 @@ function print0wing(invoice) {
     today.getDate() + 30
   );
 
-  // 세부 사항을 출력한다.
-  console.log(`고객맹: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    console.log(`고객맹: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
+}
+
+function printBanner() {
+  console.log("***************");
+  console.log("**** 고객 채무 ****");
+  console.log("***************");
 }
 
 print0wing(MockInvoice);
