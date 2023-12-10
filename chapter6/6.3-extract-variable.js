2373b5458a6d70a0f2884ba3723c34e840b025d0
KANGHO <ariaskkh@gmail.com>
2023-12-10 16:26:03 +0900

    6.3 클래스 예시 생성

--- chapter6/6.3-extract-variable.js
+++ chapter6/6.3-extract-variable.js
@@ -1,3 +1,4 @@
+// ex1
 function price(order) {
   // 가격(price) = 기본 가격 - 수량 할인 + 배송비
   const basePrice = order.quantity * order.itemPrice;
@@ -5,4 +6,24 @@ function price(order) {
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping;
+}
+
+// ex2
+class order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get qunatity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    order.quantity * order.itemPrice -
+      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+      Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  }
 }
