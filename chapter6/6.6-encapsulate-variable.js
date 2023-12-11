4b584e91b463d730067170aed25c6bb98f22bf60
KANGHO <ariaskkh@gmail.com>
2023-12-12 00:04:36 +0900

    6.6.2 값 캡슐화 생성

--- chapter6/6.6-encapsulate-variable.js
+++ chapter6/6.6-encapsulate-variable.js
@@ -1,6 +1,8 @@
 // 변수 캡슐화하기
 
-//ex1)
+import { assert } from "console";
+
+// ex1
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 function getDefaultOwner() {
   return defaultOwner;
@@ -11,3 +13,10 @@ function setDefaultOwner(arg) {
 
 spaceship.owner = getDefaultOwner();
 setDefaultOwner({ firstName: "하이", lastName: "찌노리" });
+
+// ex2 값 캡슐화하기
+const owner1 = defaultOwner();
+assert.equal("파울러", owner1.lastName, "처음 값 확인");
+const owner2 = defaultOwner();
+owner2.lastName = "파슨스";
+assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후"); // 성공할까?
