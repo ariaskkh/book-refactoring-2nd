0e8cdc7fa227d1d33bfd6286a0ae4e0695f4b580
KANGHO <ariaskkh@gmail.com>
2023-12-12 00:13:31 +0900

    6.7 캡슐화하여 처리

--- chapter6/6.7-rename-variable.js
+++ chapter6/6.7-rename-variable.js
@@ -1,5 +1,12 @@
 // 변수 이름 바꾸기
 
 let tpHd = "untitled";
-result = `<h1>${tpHd}</h1>`;
-tpHd = obj["articleTitle"];
+result = `<h1>${title}</h1>`;
+setTitle(obj["articleTitle"]);
+
+function title() {
+  return tpHd;
+}
+function setTitle(arg) {
+  return (tpHd = arg);
+}
