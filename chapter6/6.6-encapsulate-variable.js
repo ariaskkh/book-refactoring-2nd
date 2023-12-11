202c824d04c483e0fac20ebdb4fb8c4f2e863d47
KANGHO <ariaskkh@gmail.com>
2023-12-12 00:07:28 +0900

    6.6.2 값 캡슐화 레코드 캡슐화하기

--- chapter6/6.6-encapsulate-variable.js
+++ chapter6/6.6-encapsulate-variable.js
@@ -5,10 +5,24 @@ import { assert } from "console";
 // ex1
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 function getDefaultOwner() {
-  return Object.assign({}, defaultOwner);
+  return new Person(defaultOwner);
 }
 function setDefaultOwner(arg) {
   return (defaultOwner = arg);
+}
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+  get firstName() {
+    return this._firstName;
+  }
 }
 
 spaceship.owner = getDefaultOwner();
