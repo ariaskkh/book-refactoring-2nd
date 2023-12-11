ea2180a8ff94eec25a15425219ab6da8ca27cfbb
KANGHO <ariaskkh@gmail.com>
2023-12-11 23:35:03 +0900

    6.5.2 함수 추출 및 매개변수 추가

--- chapter6/6.5-change-function-declaration.js
+++ chapter6/6.5-change-function-declaration.js
@@ -1,5 +1,7 @@
 // 함수 선언 바꾸기
 
+import { assert } from "console";
+
 // ex1
 function circumference(radius) {
   return 2 * Math.PI * radius;
@@ -8,6 +10,11 @@ function circumference(radius) {
 // ex2
 class Book {
   addReservation(customer) {
+    this.zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
 }
