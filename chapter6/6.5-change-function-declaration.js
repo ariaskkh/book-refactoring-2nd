baddc49b34a62af3445f75d13a6189eddc845f7b
KANGHO <ariaskkh@gmail.com>
2023-12-11 23:33:02 +0900

    6.5.2 매개변수 추가하기 예제 생성

--- chapter6/6.5-change-function-declaration.js
+++ chapter6/6.5-change-function-declaration.js
@@ -1,5 +1,13 @@
 // 함수 선언 바꾸기
 
+// ex1
 function circumference(radius) {
   return 2 * Math.PI * radius;
+}
+
+// ex2
+class Book {
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
 }
