eabd3d762f3ba2b8d002c1f4b351056814a6225f
KANGHO <ariaskkh@gmail.com>
2023-12-12 00:02:29 +0900

    6.6.1 캡슐화

--- chapter6/6.6-encapsulate-variable.js
+++ chapter6/6.6-encapsulate-variable.js
@@ -2,6 +2,12 @@
 
 //ex1)
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+function getDefaultOwner() {
+  return defaultOwner;
+}
+function setDefaultOwner(arg) {
+  return (defaultOwner = arg);
+}
 
-spaceship.owner = defaultOwner;
-defaultOwner = { firstName: "하이", lastName: "찌노리" };
+spaceship.owner = getDefaultOwner();
+setDefaultOwner({ firstName: "하이", lastName: "찌노리" });
