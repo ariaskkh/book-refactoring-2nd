b88c2e9620ff307dda074056d947b806d467bb4d
KANGHO <ariaskkh@gmail.com>
2023-12-12 08:42:24 +0900

    6.9 class에 함수 이동하여 client3 수정

--- chapter6/6.9-combine-functions-into-class.js
+++ chapter6/6.9-combine-functions-into-class.js
@@ -18,3 +18,28 @@ const basicChargeAmount = calculateBaseCharge(aReading);
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
+
+class Reading {
+  _customer;
+  _quantity;
+  _month;
+  _year;
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data._month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
