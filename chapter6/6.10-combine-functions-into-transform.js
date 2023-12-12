c1ead6839ac2ee7439393c6dbb79366336f3d093
KANGHO <ariaskkh@gmail.com>
2023-12-13 00:02:43 +0900

    6.10 client1, 2에 변환 함수 적용

--- chapter6/6.10-combine-functions-into-transform.js
+++ chapter6/6.10-combine-functions-into-transform.js
@@ -1,26 +1,43 @@
 // 여러 함수를 변환 함수로 묶기
 
+import { assert } from "console";
+
 // ex1
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 // client1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // client2
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-const texableCharge = Math.max(0, base - taxThreshold(aReading.yaer));
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const texableCharge = Math.max(
+  0,
+  aReading.baseCharge - taxThreshold(aReading.yaer)
+);
 
 // client3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
+const rawReading = acquireReading();
+const aReading = erichReading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
 
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.yaer) * aReading.quantity;
 }
 
 function enrichReading(original) {
+  // 본질은 같고 부가 정보만 덧붙이는 변환 함수 이름을 enrich라 함.
   const result = _.clonDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
   return result;
 }
+
+// test code
+it("check reading unchaged", function () {
+  const baseReading = { customer: "ivan", quantity: 15, month: 5, year: 2017 };
+  const oracle = _.cloneDeep(baseReading);
+  enrichReading(baseReading);
+  assert.deepEqual(baseReading, oracle);
+});
