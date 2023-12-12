e18fa73e3f5f852b69a0be1b49eb8b54136b8fb6
KANGHO <ariaskkh@gmail.com>
2023-12-13 00:24:26 +0900

    6.11 함수 추출 및 데이터 구조 반환

--- chapter6/6.11-split-phase.js
+++ chapter6/6.11-split-phase.js
@@ -2,18 +2,22 @@
 
 // ex1 상품 결제 금액 계산 코드
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity); // 중간 데이터 구조
+  return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
+  // 첫 번째 단계를 처리하는 함수
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = {
+  return {
     basePrice: basePrice,
     quantity: quantity,
     discount: discount,
-  }; // 중간 데이터 구조
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  };
 }
 
 function applyShipping(priceData, shippingMethod) {
