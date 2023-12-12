918106aa062c81d43c25a3d2b7f886db09f0432b
KANGHO <ariaskkh@gmail.com>
2023-12-13 00:18:53 +0900

    6.11 priceData 중간 데이터 구조 생성

--- chapter6/6.11-split-phase.js
+++ chapter6/6.11-split-phase.js
@@ -7,6 +7,24 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const priceData = {}; // 중간 데이터 구조
+  const price = applyShipping(
+    priceData,
+    basePrice,
+    shippingMethod,
+    quantity,
+    discount
+  );
+  return price;
+}
+
+function applyShipping(
+  priceData,
+  basePrice,
+  shippingMethod,
+  quantity,
+  discount
+) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
