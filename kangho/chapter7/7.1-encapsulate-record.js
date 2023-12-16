41d0d50afff5e0e9ebf7ba5aad6bbae24a177fc1
KANGHO <ariaskkh@gmail.com>
2023-12-16 10:33:20 +0900

    7.1 객체로 만들기

--- kangho/chapter7/7.1-encapsulate-record.js
+++ kangho/chapter7/7.1-encapsulate-record.js
@@ -1,11 +1,23 @@
 // 레코드 캡슐화하기
 
 //ex1
-const organization = { name: "대대손손 진호형 옷가게", country: "CN" };
+const organization = new Organization({
+  name: "대대손손 진호형 옷가게",
+  country: "CN",
+});
+
+function getRawDataOfOrganization() {
+  return organization.data;
+}
+function getOrganization() {
+  return organization;
+}
+
+class Organization {
+  constructor(data) {
+    this_.data = data;
+  }
+}
 
 result += `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
 getRawDataOfOrganization().name = newName; // 쓰기 예
-
-function getRawDataOfOrganization() {
-  return organization;
-}
