1f0de40f318ac85cf2001e64f3ed937f73ac4fa1
KANGHO <ariaskkh@gmail.com>
2023-12-17 00:00:59 +0900

    7.4 변수 캡슐화

--- kangho/chapter7/7.3-replace-primitive-with-object.js
+++ kangho/chapter7/7.3-replace-primitive-with-object.js
@@ -3,8 +3,14 @@
 // ex1
 class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this._priority = data.priority;
     // 나머지 초기화 코드 생략
+  }
+  get priority() {
+    return this._priority;
+  }
+  set priority(aString) {
+    this._priority = aString;
   }
 }
 
