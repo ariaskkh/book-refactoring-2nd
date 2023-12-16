0d06c54242517373704e172928f1a8e9ad43069f
KANGHO <ariaskkh@gmail.com>
2023-12-17 00:28:43 +0900

    7.4 Priority 클래스에 새로운 동작 담기

--- kangho/chapter7/7.3-replace-primitive-with-object.js
+++ kangho/chapter7/7.3-replace-primitive-with-object.js
@@ -6,6 +6,9 @@ class Order {
     this._priority = data.priority;
     // 나머지 초기화 코드 생략
   }
+  get priority() {
+    return this._priority;
+  }
   get priorityString() {
     return this._priority.toString();
   }
@@ -16,14 +19,31 @@ class Order {
 
 class Priority {
   constructor(value) {
-    this._value = value;
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`<${value}는 유효하지 않은 우선순위입니다.`);
   }
   toString() {
     return this._value;
   }
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+  equals(other) {
+    return (this._index = other._index);
+  }
+  higherThan(other) {
+    return this._index > other._index;
+  }
+  lowerThan(other) {
+    return this._index < other._index;
+  }
 }
 
 // client
-highPriorityCount = orders.filter(
-  (o) => "high" === o.priorityString || "rush" === o.priorityString
+highPriorityCount = orders.filter((o) =>
+  o.priority.heigherThan(new Priority("normal"))
 ).length;
