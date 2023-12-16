c69b88bbd488df36c1df89628df271343aa449e6
KANGHO <ariaskkh@gmail.com>
2023-12-16 10:31:32 +0900

    7.1 상수 캡슐화

--- kangho/chapter7/7.1-encapsulate-record.js
+++ kangho/chapter7/7.1-encapsulate-record.js
@@ -3,5 +3,9 @@
 //ex1
 const organization = { name: "대대손손 진호형 옷가게", country: "CN" };
 
-result += `<h1>${organization.name}</h1>`; // 읽기 예
-organization.name = newName; // 쓰기 예
+result += `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
+getRawDataOfOrganization().name = newName; // 쓰기 예
+
+function getRawDataOfOrganization() {
+  return organization;
+}
