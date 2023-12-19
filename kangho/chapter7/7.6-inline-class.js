b830e9dd50f86b26873bfa990ecefe830a1b1505
KANGHO <ariaskkh@gmail.com>
2023-12-19 23:43:19 +0900

    7.6 shipmment 클래스로 필드 옮기기 + TrackingInformation class 제거

--- kangho/chapter7/7.6-inline-class.js
+++ kangho/chapter7/7.6-inline-class.js
@@ -1,10 +1,15 @@
 // 클래스 인라인하기
 
 // ex1
-class TrackingInformation {
+
+class Shipment {
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
+  // 위임 함수 생성
   get shippingCompany() {
     return this._shippingCompany;
-  } // 배송 회사
+  }
   set shippingCompany(arg) {
     this._shippingCompany = arg;
   }
@@ -13,28 +18,6 @@ class TrackingInformation {
   }
   set trackingNumber(arg) {
     this._trackingNumber = arg;
-  }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
-class Shipment {
-  get trackingInfo() {
-    return this._trackingInformation.display;
-  }
-  get trackingInformation() {
-    return this._trackingInformation;
-  }
-  set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation;
-  }
-  // 위임 함수 생성
-  get shippingCompany() {
-    return this._trackingInformation.shippingCompany;
-  }
-  set shippingCompany(arg) {
-    this._trackingInformation.shippingCompany = arg;
   }
 }
 
