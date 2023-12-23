280467ae299e196d1b0b7c7514a241ede352f9f7
KANGHO <ariaskkh@gmail.com>
2023-12-23 21:22:39 +0900

    8.1.2 다른 클래스로 옮기기 예제 생성

--- kangho/chapter8/8.1-move-function.js
+++ kangho/chapter8/8.1-move-function.js
@@ -37,3 +37,24 @@ function distance(p1, p2) {
 function radians(degrees) {
   return (degrees * Math.PI) / 180;
 }
+
+// ex2 다른 클래스로 옮기기
+class Account {
+  // 은행 이자 계산
+  get bankCharge() {
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  // 초과 인출 이자 계산
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this.daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+    } else return this.daysOverdrawn * 1.75;
+  }
+}
+
+class Accountype {}
