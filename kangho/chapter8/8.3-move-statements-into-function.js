77a97275a37ffe441143a3244b019b675eb8ec4f
KANGHO <ariaskkh@gmail.com>
2023-12-25 15:21:12 +0900

    8.3 인라인 후 이름 변경

--- kangho/chapter8/8.3-move-statements-into-function.js
+++ kangho/chapter8/8.3-move-statements-into-function.js
@@ -5,20 +5,17 @@ function renderPerson(outStream, person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(zznew(person.photo));
+  result.push(emitPhotoData(person.photo));
   return result.join("\n");
 }
 function photoDiv(p) {
-  return ["<div>", zznew(p), "</div>"].join("\n");
+  return ["<div>", emitPhotoData(p), "</div>"].join("\n");
 }
 
-function zznew(p) {
-  return [`<p>제목: ${p.title}</p>`, emitPhotoData(p)].join("\n");
-}
-
-function emitPhotoData(aPhoto) {
-  const result = [];
-  result.push(`<p>위치: ${aPhoto.location}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-  return result.join("\n");
+function emitPhotoData(p) {
+  return [
+    `<p>제목: ${p.title}</p>`,
+    `<p>위치: ${aPhoto.location}</p>`,
+    `<p>날짜: ${aPhoto.date.toDateString()}</p>`,
+  ].join("\n");
 }
