a740c1aff6a01fd8e65e9d1bccb977a5aa55be31
KANGHO <ariaskkh@gmail.com>
2023-12-25 15:20:00 +0900

    8.3 zznew()로 대체

--- kangho/chapter8/8.3-move-statements-into-function.js
+++ kangho/chapter8/8.3-move-statements-into-function.js
@@ -5,14 +5,15 @@ function renderPerson(outStream, person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`);
-  result.push(emitPhotoData(person.photo));
+  result.push(zznew(person.photo));
   return result.join("\n");
 }
 function photoDiv(p) {
-  return ["<div>", `<p>제목: ${p.title}</p>`, emitPhotoData(p), "</div>"].join(
-    "\n"
-  );
+  return ["<div>", zznew(p), "</div>"].join("\n");
+}
+
+function zznew(p) {
+  return [`<p>제목: ${p.title}</p>`, emitPhotoData(p)].join("\n");
 }
 
 function emitPhotoData(aPhoto) {
