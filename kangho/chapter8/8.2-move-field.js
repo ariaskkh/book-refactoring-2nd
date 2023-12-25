2cde11718fad462b70c81353ee7f5236d49b353d
KANGHO <ariaskkh@gmail.com>
2023-12-25 14:50:45 +0900

    8.2 discountRate 필드를 Customer에서 CustomerContract로 옮기기

--- kangho/chapter8/8.2-move-field.js
+++ kangho/chapter8/8.2-move-field.js
@@ -4,24 +4,35 @@
 class Customer {
   constructor(name, discountRate) {
     this._name = name;
-    this._discountRate = discountRate;
     this._contract = new CustomerContract(dateToday());
+    this._setDiscountRate(discountRate);
   }
 
   get discountRate() {
     return this._discountRate;
   }
+  _setDiscountRate(aNumber) {
+    this._discountRate = aNumber;
+  }
   becomePreferred() {
-    this._discountRate += 0.03;
+    this._setDiscountRate(this_discountRate + 0.03);
     // 다른 멋진 일들
   }
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._discountRate));
+    return amount.subtract(amount.multiply(this.discountRate));
   }
 }
 
 class CustomerContract {
-  constructor(startDate) {
+  constructor(startDate, discountRate) {
     this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+  set discountRate(arg) {
+    this._discountRate = arge;
   }
 }
