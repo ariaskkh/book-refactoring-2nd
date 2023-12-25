2612af1c963ad7e96bb16bd0b2d3938b0186c31a
KANGHO <ariaskkh@gmail.com>
2023-12-25 14:59:22 +0900

    8.2.2 공유 객체로 이동하기 예제 생성

--- kangho/chapter8/8.2-move-field.js
+++ kangho/chapter8/8.2-move-field.js
@@ -36,3 +36,22 @@ class CustomerContract {
     this._discountRate = arge;
   }
 }
+
+// ex2 공유 객체로 이동하기
+class Account {
+  constructor(number, type, interestRate) {
+    this._number = number;
+    this._type = type;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
+  }
+}
+
+class AccountType {
+  constructor(nameString) {
+    this._name = nameString;
+  }
+}
