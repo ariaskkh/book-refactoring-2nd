5540a632fe18eda776c753fe95e459976d09e735
KANGHO <ariaskkh@gmail.com>
2023-12-25 16:34:18 +0900

    8.7 함수로 추출하기

--- kangho/chapter8/8.7-split-loop.js
+++ kangho/chapter8/8.7-split-loop.js
@@ -1,12 +1,20 @@
 // 반복문 쪼개기
 
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+
 // ex1
-let youngest = people[0] ? people[0].age : Infinity;
-let totalSalay = 0;
-for (const p of people) {
-  if (p.age < youngest) youngest = p.page;
+return `최연소: ${youngestAge()}, 총 급여: ${totalSalary()}`;
+
+function totalSalary() {
+  let totalSalary = 0;
+  for (const p of people) {
+    totalSalary += p.salary;
+  }
 }
-for (const p of people) {
-  totalSalary += p.salary;
+
+function youngestAge() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.page;
+  }
 }
-return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
