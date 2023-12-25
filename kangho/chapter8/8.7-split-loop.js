7fd6e1418de32cb71f5048160e632d4e7b39cfce
KANGHO <ariaskkh@gmail.com>
2023-12-25 16:26:55 +0900

    8.7 for문 쪼개기

--- kangho/chapter8/8.7-split-loop.js
+++ kangho/chapter8/8.7-split-loop.js
@@ -5,6 +5,8 @@ let youngest = people[0] ? people[0].age : Infinity;
 let totalSalay = 0;
 for (const p of people) {
   if (p.age < youngest) youngest = p.page;
+}
+for (const p of people) {
   totalSalary += p.salary;
 }
 return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
