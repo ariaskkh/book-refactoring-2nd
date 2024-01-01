97537f378a01db20b80b229045950327dd66e1e1
KANGHO <ariaskkh@gmail.com>
2024-01-01 16:53:38 +0900

    9.5 저장소 객체 생성하여  id 하나당 고객 객체 하나만 생성

--- kangho/chapter9/9.5-change-value-to-reference.js
+++ kangho/chapter9/9.5-change-value-to-reference.js
@@ -3,7 +3,7 @@
 class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customer); // data.customer가 고객 ID
+    this._customer = registerCustomer(data.customer); // data.customer가 고객 ID
     // 다른 데이터를 읽어 들인다.
   }
   get customer() {
@@ -19,4 +19,22 @@ class Customer {
   get id() {
     return this._id;
   }
+}
+
+// 저장소 객체
+let _repositoryData;
+
+export function initialize() {
+  _repositoryData = {};
+  _repositoryData.customers = new Map();
+}
+
+export function registerCustomer(id) {
+  if (!_repositoryData.customers.has(id))
+    _repositoryData.customers.set(id, new Customer(id));
+  return findCustomer(id);
+}
+
+export function findCustomer(id) {
+  return _repositoryData.customer.get(id);
 }
