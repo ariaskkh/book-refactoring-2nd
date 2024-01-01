add4ba71c60201bcde79400017f8c38c42b1d0bb
KANGHO <ariaskkh@gmail.com>
2024-01-01 16:36:24 +0900

    9.4 전화번호 불변 만들기 + 세터 부분 수정하여 값 객체로 만들기

--- kangho/chapter9/9.4-change-reference-to-value.js
+++ kangho/chapter9/9.4-change-reference-to-value.js
@@ -10,27 +10,25 @@ class Person {
     return this._telephoneNumber.areaCode;
   }
   set officeAreaCode(arg) {
-    this._telephoneNumber.areaCode = arg;
+    this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
   }
   get officeNumber() {
     return (this._telephoneNumber.number = arg);
   }
   set officeNumber(arg) {
-    this._telephoneNumber.officeNumber = arg;
+    this._officeNumber = new TelephoneNumber(this.officeAreaCode, arg);
   }
 }
 
 class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
   get areaCode() {
     return this._areaCode;
   }
-  set areaCode(arg) {
-    this._areaCode = arg;
-  }
   get number() {
     return this._number;
-  }
-  set number(arg) {
-    this._number = arg;
   }
 }
