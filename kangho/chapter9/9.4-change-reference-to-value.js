318ff166f50231a28462cb9deec7447065956572
KANGHO <ariaskkh@gmail.com>
2024-01-01 16:32:02 +0900

    9.4 참조를 값으로 바꾸기 예제 생성

---
+++ kangho/chapter9/9.4-change-reference-to-value.js
@@ -0,0 +1,36 @@
+// 참조를 값으로 바꾸기
+
+// ex1
+class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.areaCode;
+  }
+  set officeAreaCode(arg) {
+    this._telephoneNumber.areaCode = arg;
+  }
+  get officeNumber() {
+    return (this._telephoneNumber.number = arg);
+  }
+  set officeNumber(arg) {
+    this._telephoneNumber.officeNumber = arg;
+  }
+}
+
+class TelephoneNumber {
+  get areaCode() {
+    return this._areaCode;
+  }
+  set areaCode(arg) {
+    this._areaCode = arg;
+  }
+  get number() {
+    return this._number;
+  }
+  set number(arg) {
+    this._number = arg;
+  }
+}
