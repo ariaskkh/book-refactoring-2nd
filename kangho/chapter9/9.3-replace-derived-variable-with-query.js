ffda9fbd89c67c3d17bf96721040665164e3c284
KANGHO <ariaskkh@gmail.com>
2024-01-01 15:58:27 +0900

    9.3 production 코드 제거

--- kangho/chapter9/9.3-replace-derived-variable-with-query.js
+++ kangho/chapter9/9.3-replace-derived-variable-with-query.js
@@ -5,15 +5,10 @@ const { assert } = require("console");
 // ex1
 class ProductionPlan {
   get production() {
-    return this.calculatedProduction;
-  }
-
-  get calculatedProduction() {
     return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
 
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
-    this._production += anAdjestment.amount;
   }
 }
