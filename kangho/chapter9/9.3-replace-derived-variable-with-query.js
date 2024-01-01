f24b01dc3113eaca661b6c9dd7b9861468b8e719
KANGHO <ariaskkh@gmail.com>
2024-01-01 15:57:31 +0900

    9.3 과정 1

--- kangho/chapter9/9.3-replace-derived-variable-with-query.js
+++ kangho/chapter9/9.3-replace-derived-variable-with-query.js
@@ -1,10 +1,17 @@
 // 파생 변수를 질의 함수로 바꾸기
 
+const { assert } = require("console");
+
 // ex1
-class productionPlan {
+class ProductionPlan {
   get production() {
-    return this._production;
+    return this.calculatedProduction;
   }
+
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._production += anAdjestment.amount;
