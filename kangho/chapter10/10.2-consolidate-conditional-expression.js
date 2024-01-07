43949f8dfa436aa9dcc0cb7b43be7183220db943
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:34:44 +0900

    10.1 or 연산자 사용하여 함수 추출

--- kangho/chapter10/10.2-consolidate-conditional-expression.js
+++ kangho/chapter10/10.2-consolidate-conditional-expression.js
@@ -2,8 +2,14 @@
 
 //ex1) or 사용하기
 function disabilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
+  if (isNotEligibleForDisability()) return 0;
   // 장애 수당 계산
+}
+
+function isNotEligibleForDisability() {
+  return (
+    anEmployee.seniority < 2 ||
+    anEmployee.monthsDisabled > 12 ||
+    anEmployee.isPartTime
+  );
 }
