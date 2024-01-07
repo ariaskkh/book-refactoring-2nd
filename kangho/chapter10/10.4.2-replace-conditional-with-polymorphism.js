70e610b721df3ed09c26fa0d9b702f216a4f4acb
KANGHO <ariaskkh@gmail.com>
2024-01-07 14:50:47 +0900

    10.4.2 client 코드 추가

--- kangho/chapter10/10.4.2-replace-conditional-with-polymorphism.js
+++ kangho/chapter10/10.4.2-replace-conditional-with-polymorphism.js
@@ -50,3 +50,14 @@ function voyageProfitFactor(voyage, history) {
   }
   return result;
 }
+
+// client
+const voyage = { zone: "서인도", length: 10 };
+const history = [
+  { zone: "동인도", profit: 5 },
+  { zone: "서인도", profit: 15 },
+  { zone: "중국", profit: -2 },
+  { zone: "서아프리카, profit: 7" },
+];
+
+const myRating = rating(voyage, history);
