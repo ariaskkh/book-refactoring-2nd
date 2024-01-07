5d1972cf684c8f4817c614aced2248674237d265
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:31:26 +0900

    10.2 조건식 통합하기 예제 생성

---
+++ kangho/chapter10/10.2-consolidate-conditional-expression.js
@@ -0,0 +1,9 @@
+// 조건식 통합하기
+
+//ex1) or 사용하기
+function disabilityAmount(anEmployee) {
+  if (anEmployee.seniority < 2) return 0;
+  if (anEmployee.monthsDisabled > 12) return 0;
+  if (anEmployee.isPartTime) return 0;
+  // 장애 수당 계산
+}
