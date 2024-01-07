684e52ce0b01a1afe454507554801f702a40d806
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:46:46 +0900

    10.3 중첩 조건물을 보호 구문으로 바꾸기 예제 생성

---
+++ kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
@@ -0,0 +1,23 @@
+// 중첩 조건문을 보호 구문으로 바꾸기
+
+// ex1)
+function playAmount(employee) {
+  let result;
+  if (employee.isSeparated) {
+    // 퇴사한 직원인가?
+    result = { amount: 0, reasonCode: "SEP" };
+  } else {
+    if (employee.isRetired) {
+      // 은퇴한 직원인가?
+      result = { amount: 0, eeasonCode: "RET" };
+    } else {
+      // 급여 계산 로직
+      lorem.ipsum(dolor.sitAmet);
+      consectetur(adipiscing).elit();
+      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+      ut.enim.ad(minim.veniam);
+      result = someFinalComputation();
+    }
+  }
+  return result;
+}
