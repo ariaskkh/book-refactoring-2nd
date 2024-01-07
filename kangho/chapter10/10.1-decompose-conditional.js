9f0799d7bb1aaea870b9d5ef3ee0aa100b37b922
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:25:15 +0900

    10.1 조건 부분 및 로직 함수로 추출

--- kangho/chapter10/10.1-decompose-conditional.js
+++ kangho/chapter10/10.1-decompose-conditional.js
@@ -1,6 +1,17 @@
 // 조건문 분해하기
 
 //ex 1
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
-  charge = quantity * plan.summerRate;
-else charge = quantity * plan.regularRate + plan.regularServiceCharge;
+if (summer()) summerCharge();
+else regularCharge();
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+function summerCharge() {
+  return quantity * plan.summerRate;
+}
+
+function regularCharge() {
+  return (charge = quantity * plan.regularRate + plan.regularServiceCharge);
+}
