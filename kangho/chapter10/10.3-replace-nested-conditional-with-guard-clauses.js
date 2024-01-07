3cada13044b5eec84e4de8f7d4e48d33fded45f2
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:49:39 +0900

    10.3 최상위 조건부터 보호 구문으로 변경(early return)

--- kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
+++ kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
@@ -5,19 +5,18 @@ function playAmount(employee) {
   let result;
   if (employee.isSeparated) {
     // 퇴사한 직원인가?
-    result = { amount: 0, reasonCode: "SEP" };
-  } else {
-    if (employee.isRetired) {
-      // 은퇴한 직원인가?
-      result = { amount: 0, eeasonCode: "RET" };
-    } else {
-      // 급여 계산 로직
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
+    return { amount: 0, reasonCode: "SEP" };
   }
+  if (employee.isRetired) {
+    // 은퇴한 직원인가?
+    return { amount: 0, eeasonCode: "RET" };
+  }
+
+  // 급여 계산 로직
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(minim.veniam);
+  result = someFinalComputation();
   return result;
 }
