cbd924cb174c07d7facf660251ab0b2b843b4a61
KANGHO <ariaskkh@gmail.com>
2024-01-07 12:02:03 +0900

    10.3.2 조건식 통합 및 return 변수 제거

--- kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
+++ kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
@@ -21,16 +21,15 @@ function playAmount(employee) {
 
 // ex2) 조건 반대로 만들기
 function adjustedCapital(anInstrument) {
-  let result = 0;
-  if (anInstrument.capital <= 0) {
-    return result;
+  if (
+    anInstrument.capital <= 0 ||
+    !anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  ) {
+    return 0;
   }
-  if (!anInstrument.interestRate <= 0 || anInstrument.duration <= 0) {
-    return result;
-  }
-  result =
+  return (
     (anInstrument.income / anInstrument.duration) *
-    anInstrument.adjustmentFactor;
-
-  return result;
+    anInstrument.adjustmentFactor
+  );
 }
