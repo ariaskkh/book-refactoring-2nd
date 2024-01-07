b3573af11055111e7a1c1faa996948bcd5df06e3
KANGHO <ariaskkh@gmail.com>
2024-01-07 15:16:35 +0900

    수정

--- kangho/chapter10/10.4.2-replace-conditional-with-polymorphism.js
+++ kangho/chapter10/10.4.2-replace-conditional-with-polymorphism.js
@@ -4,85 +4,84 @@
 
 // 투자 등급
 function rating(voyage, history) {
-  return createRating(voyage, history).value
+  return createRating(voyage, history).value;
 }
 
 // 함수들을 Rating 클래스로 묶었다.
 class Rating {
-    constructor(voyage, history){
-        this.voyage = voyage;
-        this.history = history;
-    }
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
+  }
 
-    get value() {
-        const vpf = voyageProfitFactor(voyage, history);
-        const vr = voyageRisk(voyage);
-        const chr = captainHistoryRisk(voyage, history);
-        if (vpf * 3 > vr + chr * 2) return "A";
-        else return "B";
-    }
+  get value() {
+    const vpf = voyageProfitFactor(voyage, history);
+    const vr = voyageRisk(voyage);
+    const chr = captainHistoryRisk(voyage, history);
+    if (vpf * 3 > vr + chr * 2) return "A";
+    else return "B";
+  }
 
-    // 항해 경로 위험요소
-    get voyageRisk() {
-        let result = 1;
-        if (voyage.length > 4) result += 2;
-        if (voyage.length > 8) result += voyage.length - 8;
-        if (["중국", "동인도"].includes(voyage.zone)) result += 4;
-        return Math.max(result, 0);
-    }
+  // 항해 경로 위험요소
+  get voyageRisk() {
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length > 8) result += voyage.length - 8;
+    if (["중국", "동인도"].includes(voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
 
-    // 선장의 항해 이력 위험 요소
-    get captainHistoryRisk() {
-        let result = 1;
-        if (history.length < 5) result += 4;
-        result += history.filter((v) => v.profit < 0).length;
-        return Math.max(result, 0);
-    }
+  // 선장의 항해 이력 위험 요소
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter((v) => v.profit < 0).length;
+    return Math.max(result, 0);
+  }
 
-    // 중국을 경유하는가?
-    get hasChina(history) {
-    return history.some((v) => "중국" === v.zone);
-    }
+  // 중국을 경유하는가?
+  get hasChina() {
+    return this.history.some((v) => "중국" === v.zone);
+  }
 
-    // 수익 요인
-    get voyageProfitFactor() {
-        let result = 2;
-        if (voyage.zone === "중국") result += 1;
-        if (voyage.zone === "동인도") result += 1;
-        result += voyageAndHistoryLengthFactor;
-        return result;
-    }
+  // 수익 요인
+  get voyageProfitFactor() {
+    let result = 2;
+    if (this.voyage.zone === "중국") result += 1;
+    if (this.voyage.zone === "동인도") result += 1;
+    result += this.voyageAndHistoryLengthFactor;
+    return result;
+  }
 
-    get voyageAndHistoryLengthFactor() {
-        let result = 0;
-        if (history.length > 8) result += 1;
-        if (history.length > 14) result -= 1;
-        return result
-    }
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    if (this.history.length > 8) result += 1;
+    if (this.voyage.length > 14) result -= 1;
+    return result;
+  }
 }
-
 
 class ExperiencedChinaRating extends Rating {
-    // 선장의 항해 이력 위험 요소
-    get captainHistoryRisk() {
-        const result = super.captainHistoryRisk - 2;
-        return Math.max(result, 0)
-    }
+  // 선장의 항해 이력 위험 요소
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
 
-    get voyageAndHistoryLengthFactor() {
-        let result = 0;
-        result += 3;
-        if (history.length > 10) result += 1;
-        if (history.length > 12) result += 1;
-        if (history.length > 18) result -= 1;
-        return result;
-    }
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    result += 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
 }
 
-function createRating(voyage, history){
-    if (voyage.zone === "중국" && history.some((v) => "중국" === v.zone))
+function createRating(voyage, history) {
+  if (voyage.zone === "중국" && history.some((v) => "중국" === v.zone))
     return new ExperiencedChinaRating(voyage, history);
-    else return new Rating(voyage, history)
+  else return new Rating(voyage, history);
 }
 
 // client
