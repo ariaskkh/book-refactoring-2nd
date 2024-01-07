e37ab144b640903d172cac7a9341be93221a3747
KANGHO <ariaskkh@gmail.com>
2024-01-07 11:52:09 +0900

    10.3.2 조건 반대로 만들기 예제 생성

--- kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
+++ kangho/chapter10/10.3-replace-nested-conditional-with-guard-clauses.js
@@ -18,3 +18,16 @@ function playAmount(employee) {
   ut.enim.ad(minim.veniam);
   return someFinalComputation();
 }
+
+// ex2) 조건 반대로 만들기
+function adjustedCapital(anInstrument) {
+  let result = 0;
+  if (anInstrument.capital > 0) {
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+      result =
+        (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor;
+    }
+  }
+  return result;
+}
