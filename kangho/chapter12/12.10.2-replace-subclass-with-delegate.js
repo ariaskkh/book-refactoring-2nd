85ca885c38634b486819787d3a4b26a58ba9b57f
KANGHO <ariaskkh@gmail.com>
2024-02-04 16:46:31 +0900

    12.10.2 위임클래스의 슈퍼클래스 생성

--- kangho/chapter12/12.10.2-replace-subclass-with-delegate.js
+++ kangho/chapter12/12.10.2-replace-subclass-with-delegate.js
@@ -14,14 +14,10 @@ class Bird {
     return this._name;
   }
   get plumage() {
-    return this._speciesDelegate
-      ? this._speciesDelegate.plumage // 이러면 plumage 없는 서브클래스에서 에러 남
-      : this._plumage || "보통이다";
+    return this._speciesDelegate.plumage;
   }
   get airSppedVelocity() {
-    return this._speciesDelegate
-      ? this._speciesDelegate.airSpeedVelocity
-      : null;
+    return this._speciesDelegate.airSpeedVelocity;
   }
 
   selectSpeciesDelegate(data) {
@@ -33,19 +29,20 @@ class Bird {
       case "노르웨이 파랑 앵무":
         return new NorwegianBlueParrotDelegate(this);
       default:
-        return null;
+        return new SpeciesDelegate(data, this);
     }
   }
 }
 
-class EuropeanSwallowDelegate {
+class EuropeanSwallowDelegate extends SpeciesDelegate {
   get airSpeedVelocity() {
     return 35;
   }
 }
 
-class AfricanSwallowDelegate {
-  constructor(data) {
+class AfricanSwallowDelegate extends SpeciesDelegate {
+  constructor(data, bird) {
+    super(data, bird);
     this._numberOfCoconuts = data._numberOfCoconuts;
   }
 
@@ -54,8 +51,9 @@ class AfricanSwallowDelegate {
   }
 }
 
-class NorwegianBlueParrotDelegate {
+class NorwegianBlueParrotDelegate extends SpeciesDelegate {
   constructor(data, bird) {
+    super(data, bird);
     this._bird = bird;
     this._voltage = data._voltage;
     this._isNailed = data._isNailed;
@@ -68,5 +66,18 @@ class NorwegianBlueParrotDelegate {
   get plumage() {
     if (this._voltage > 100) return "그을렸다";
     else return this._plumage || "예쁘다";
+  }
+}
+
+class SpeciesDelegate {
+  constructor(data, bird) {
+    this._bird = bird;
+  }
+  get plumage() {
+    return this._bird._plumage || "보통이다";
+  }
+
+  get airSpeedVelocity() {
+    return null;
   }
 }
