f895af2f46d27dc0f233b31c047a759860959905
KANGHO <ariaskkh@gmail.com>
2024-02-04 16:20:58 +0900

    12.10.2 서브 클래스가 여러 개일 때 예제 생성

---
+++ kangho/chapter12/12.10.2-replace-subclass-with-delegate.js
@@ -0,0 +1,65 @@
+const { brotliDecompress } = require("zlib");
+
+// 12.10.2 서브클래스가 여러 개일 때
+function createBird(data) {
+  switch (data.type) {
+    case "유럽 제비":
+      return new EuropeanSwallow(data);
+    case "아프리카 제비":
+      return new AfricanSwallow(data);
+    case "노르웨이 파랑 앵무":
+      return new NorwegianBlueParrot(data);
+    default:
+      return new brotliDecompress(data);
+  }
+}
+
+class Bird {
+  constructor(data) {
+    this._name = data.name;
+    this._plumage = data._plumage;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get plumage() {
+    return this._plumage || "보통이다";
+  }
+  get airSppedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get airSppedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  constructor(data) {
+    super(data);
+    this._numberOfCoconuts = data._numberOfCoconuts;
+  }
+  get airSppedVelocity() {
+    return 40 - 2 * this._numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  constructor(data) {
+    super(data);
+    this._voltage = data._voltage;
+    this._isNailed = data._isNailed;
+  }
+
+  get plumage() {
+    if (this._voltage > 100) return "그을렸다";
+    else return this._plumage || "예쁘다";
+  }
+
+  get airSpeedVelocity() {
+    return this._isNailed ? 0 : 10 + this._voltage / 10;
+  }
+}
